b3bab5cb04f5ab3d81dd9cce45548057c4d163c8
SamiFraca <fracasami@gmail.com>
2024-07-17 15:08:56 +0200

    Add basic css responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import {  ProductList,ProductListProps } from "./components/products-list";
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between md:p-24">
       <div className="z-10 w-full max-w-5xl items-center  font-mono text-sm lg:flex flex-col border rounded-md py-4 ">
         <h1 className="flex justify-center w-full text-2xl my-2">Products App</h1>
         <ControlToolbar />
